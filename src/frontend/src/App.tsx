63a916a2823cb6bcafaa993917e55443a2c1a975
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2026-05-31 01:19:54 +0200

    prueba despligue

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Sistema de Desarrollo de Software (SDVC)</h1>
+      <h1>Sistema de Desarrollo de Software (SDVC) cosorro</h1>
       <p>El frontend está configurado y listo para Cloudflare Pages.</p>
     </div>
   );
