dd246b69cd7f230ecd0391a36f0e49f3ccdf83f0
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2026-06-13 19:13:59 +0200

    feat: Dto para crear usuarios

---
+++ src/dtos/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsEmail, IsNotEmpty, IsEnum, IsOptional, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsEnum(['investigador', 'coordinador'])
+  @IsOptional()
+  role?: string;
+}
